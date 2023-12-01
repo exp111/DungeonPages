ed5b0cb40593494423c144f1514d8c0cd1136e1c
Exp <17885980+exp111@users.noreply.github.com>
2023-12-01 15:04:29 +0100

    map editor wip

--- docs/js/map.js
+++ docs/js/map.js
@@ -2,18 +2,45 @@ class Map {
     Dungeons = [];
     Monsters = [];
     Traps = [];
+    DOMObject = null;
     
+    //TODO: can we do this automatically
+    ToJson() {
+        let ret = {
+            dungeons: [],
+            monsters: [],
+            traps: [],
+        };
+        for (let i in this.Dungeons)
+        {
+            let d = this.Dungeons[i];
+            ret.dungeons.push(d.ToJson());
+        }
+        //TODO: monsters, traps
+        return ret;
+    }
+    CreateDOM() {
+        let ret = document.createElement("div");
+        ret.classList.add("map");
+        for (let i in this.Dungeons) {
+            let d = this.Dungeons[i];
+            ret.appendChild(d.DOMObject);
+        }
+        this.DOMObject = ret;
+        return ret;
+    }
     static FromJson(json) {
         let map = new Map();
 
         for (let i in json.dungeons)
         {
             let d = json.dungeons[i];
-            map.Dungeons.appendChild(d);
+            map.Dungeons.push(Dungeon.FromJson(d));
         }
+        //TODO: monsters, traps
         map.Monsters = json.monster,
         map.Traps = map.Traps;
-        //map.CreateDOM();
+        map.CreateDOM();
         return map;
     }
 }
@@ -24,10 +51,29 @@ class Dungeon {
     Rows = 6;
     Columns = 6;
     Dice = 1;
+    Width = 1;
+    Height = 1;
     Tiles = [];
     DOMObject = null;
     //TODO: dots
 
+    ToJson() {
+        let ret = {
+            "name": this.Name,
+            "rows": this.Rows,
+            "columns": this.Columns,
+            "dice": this.Dice,
+            "width": this.Width,
+            "height": this.Height,
+            "tiles": []
+        }
+        for (let i in this.Tiles)
+        {
+            let t = this.Tiles[i];
+            ret.tiles.push(t.ToJson());
+        }
+        return ret;
+    }
     CreateDOM() {
         let ret = document.createElement("div");
         ret.classList.add("dungeon");
@@ -43,6 +89,7 @@ class Dungeon {
         /// icons
         let dices = document.createElement("div");
         dices.classList.add("dungeon-dices");
+        // wandering dice
         if (this.Dice >= 2)
         {
             let wandering = document.createElement("div");
@@ -62,6 +109,8 @@ class Dungeon {
         grid.classList.add("dungeon-grid");
         grid.style.setProperty("--row-count", this.Rows);
         grid.style.setProperty("--column-count", this.Columns);
+        ret.style.setProperty("grid-column-end", `span ${this.Width}`);
+        ret.style.setProperty("grid-row-end", `span ${this.Height}`);
         // then add the tiles
         for (let i in this.Tiles)
         {
@@ -79,6 +128,10 @@ class Dungeon {
         dungeon.Name = json.name;
         dungeon.Rows = json.rows,
         dungeon.Columns = json.columns;
+        if (json.width != null)
+            dungeon.Width = json.width;
+        if (json.height != null)
+            dungeon.Height = json.height;
         if (json.boss != null)
             dungeon.isBoss = json.boss;
         dungeon.Dice = json.dice;
@@ -97,6 +150,16 @@ class Tile {
     Width = 1;
     Height = 1;
     DOMObject = null;
+
+    ToJson() {
+        let ret = {
+            "type": this.Type,
+            "subtype": this.Subtype,
+            "width": this.Width,
+            "height": this.Height
+        };
+        return ret;
+    }
 
     CreateDOM() {
         // Create tile
@@ -117,10 +180,10 @@ class Tile {
         tile.Type = json.type;
         if (json.subtype != null)
             tile.Subtype = json.subtype;
-        if (json.w != null)
-            tile.Width = json.w;
-        if (json.h != null)
-            tile.Height = json.h;
+        if (json.width != null)
+            tile.Width = json.width;
+        if (json.height != null)
+            tile.Height = json.height;
         tile.CreateDOM(tile);
         return tile;
     }
