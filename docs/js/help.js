58421b5a629eafec3abfa1755ceb0a0798c25791
Exp <17885980+exp111@users.noreply.github.com>
2024-02-01 22:41:52 +0100

    first page content

--- docs/js/help.js
+++ docs/js/help.js
@@ -1,4 +1,6 @@
 class Help {
+    BGGLink = "https://boardgamegeek.com/boardgame/374145/dungeon-pages-core-set";
+    GithubLink = "https://github.com/exp111/DungeonPages";
     // Runtime
     ButtonDOMObject = null;
     DOMObject = null;
@@ -26,6 +28,26 @@ class Help {
         close.innerText = "Close";
         close.onclick = () => Global.help.SetVisible(false);
         obj.appendChild(close);
+        /// Menu
+        let menu = document.createElement("div");
+        menu.classList.add("help-menu");
+        obj.appendChild(menu);
+        //// Header
+        let header = document.createElement("div");
+        header.classList.add("help-menu-header");
+        header.innerText = "Help";
+        menu.appendChild(header);
+        //// Content
+        let content = document.createElement("div");
+        content.classList.add("help-menu-content");
+        content.innerHTML = `<span>What is Dungeon Pages?</span></br>
+        Dungeon Pages is a one page roll-and-write board game. If you want more info, check out the <a href="${this.BGGLink}">BoardGameGeek page</a>.</br>
+        <span>How does this game work?</span></br>
+        You can start the tutorial by pressing the "Tutorial" button.</br>
+        <span>I have a question/problem/bug or I want something changed/added/removed!</span></r>
+        Please open a issue on <a href="${this.GithubLink}">GitHub</a>.
+        `;
+        menu.appendChild(content);
         this.UpdateDOM();
     }
 
